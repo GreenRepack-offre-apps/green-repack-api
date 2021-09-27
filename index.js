03d8def6773fe2224e712f35c4cf4162a82b6edf
uriel <uriel.vido@hotmail.fr>
2021-09-27 16:49:02 +0200

    rectif 2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 
-var port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send({app:{name:local_config.app}});
